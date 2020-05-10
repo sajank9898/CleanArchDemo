e9a4f2a362602be7dc428d3ea40f64358e406ab6
Sajan K Mathew <sajan.mathew@siemens.com>
2020-05-10 22:10:28 +0530

    Added Swagger and Complete Mvc and Api Projects

--- CleanArch/CleanArch.Api/Startup.cs
+++ CleanArch/CleanArch.Api/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +29,19 @@ namespace CleanArch.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CleanArchDBContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("CleanArchDBConnection"))); 
+            
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Clean Arch Api", Version = "v1" });
+                
+            }
+            );
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +54,13 @@ namespace CleanArch.Api
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean Arch Api v1");
+            }
+
+            );
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +69,12 @@ namespace CleanArch.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
